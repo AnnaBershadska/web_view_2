97b2eebae5c1deeabd0063da03b80e47f7a1725a
Anna Bershadska <anna.b@leadsdoit.net>
2023-11-27 12:29:11 +0100

    Saving correct url in shred prefs

--- lib/shared_prefs_manager.dart
+++ lib/shared_prefs_manager.dart
@@ -4,23 +4,27 @@ const String REDIRECT_URL_KEY = 'redirect_url';
 const String LAST_URL_KEY = 'last_url';
 
 class SharedPrefsManager {
-  static Future<void> saveRedirectUrl(String url) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString(REDIRECT_URL_KEY, url);
+  late final SharedPreferences _prefs;
+
+  Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
   }
 
-  static Future<String> getRedirectUrl() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString(REDIRECT_URL_KEY) ?? '';
+  Future<void> saveRedirectUrl(String url) async {
+    if (url != getLastUrl()) { //only update if changed
+      await _prefs.setString(REDIRECT_URL_KEY, url);
+    }
   }
 
-  static Future<void> saveLastUrl(String url) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString(LAST_URL_KEY, url);
+  String getRedirectUrl() {
+    return _prefs.getString(REDIRECT_URL_KEY) ?? '';
   }
 
-  static Future<String> getLastUrl() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString(LAST_URL_KEY) ?? '';
+  Future<void> saveLastUrl(String url) async {
+    await _prefs.setString(LAST_URL_KEY, url);
+  }
+
+  String getLastUrl() {
+    return _prefs.getString(LAST_URL_KEY) ?? '';
   }
 }
