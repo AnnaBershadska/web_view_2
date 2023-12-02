ce4499e225603b9d06fd6aa7f546d8757a3b1eea
Anna Bershadska <anna.b@leadsdoit.net>
2023-12-02 11:26:45 +0100

    Saving white state

--- lib/shared_prefs_manager.dart
+++ lib/shared_prefs_manager.dart
@@ -2,6 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 const String REDIRECT_URL_KEY = 'redirect_url';
 const String LAST_URL_KEY = 'last_url';
+//if true we always go to white. If false we always got to black. Defaults to true
+const String WEB_VIEW_ENABLED_KEY = 'web_view_enabled';
 
 class SharedPrefsManager {
   late final SharedPreferences _prefs;
@@ -26,5 +28,13 @@ class SharedPrefsManager {
 
   String getLastUrl() {
     return _prefs.getString(LAST_URL_KEY) ?? '';
+  }
+
+  Future<void> setWebViewEnabled(bool isEnabled) async {
+    await _prefs.setBool(WEB_VIEW_ENABLED_KEY, isEnabled);
+  }
+
+  bool getWebViewEnabled() {
+    return _prefs.getBool(WEB_VIEW_ENABLED_KEY) ?? true;
   }
 }
